cc16139b82163cd5f5a6b08e3a19b45e1c035446
Shubham Mittal <43808987+shubhammitt@users.noreply.github.com>
2020-03-04 19:46:52 +0530

    Update shell.c

--- shell.c
+++ shell.c
@@ -8,144 +8,97 @@
 #include <assert.h>
 #include <sys/stat.h>
 #define buf_size 256
-int  getcmd(char *cmd)
+int getcmd(char *cmd)
 {
 	fprintf(stdout,"$ ");
 	memset(cmd,0,buf_size);
 	fgets(cmd,buf_size,stdin);
-	if(cmd[0]==0)
+	if( cmd[0] == 0 )
 		return 0;
 	return 1;
-
 }
 void redirect(char *fin_argv[],int len)
 {
 	char *argv[buf_size];
-	for(int i=0;i<=len;i++)
-		argv[i]=fin_argv[i];
-	for(int i=0;i<=len;i++)
+	for( int i = 0; i <= len; i++ )
+		argv[i] = fin_argv[i];
+
+	for(int i = 0; i <= len ; i++)
 	{
-		int n=strlen(argv[i]);
-		if(n>=1 && argv[i][0]=='>')
+		int n = strlen(argv[i]);
+		if( n >= 1 && argv[i][0] == '>' ) //> or >>
 		{
-			if(n==1)
+			close(1);
+			if( n == 1)
 			{
-				//next i is filename
-				close(1);
 				creat(argv[i+1], 0666);
-
-				argv[i]="";
-				i++;
-				argv[i]="";
+				argv[i++] = "";
 			}
-			else if(argv[i][1]=='>')
+			else if(argv[i][1] == '>')
 			{
-				if(n==2)
+				if(n == 2)
 				{
-					close(1);
 					open(argv[i+1],O_WRONLY|O_APPEND|O_CREAT);
-
-					argv[i]="";
-					i++;
-					argv[i]="";
+					argv[i++] = "";
 				}
 				else
 				{
-					//>>input
 					char* token = strtok(argv[i], ">>");
-					 close(1);
 					open(token,O_WRONLY|O_APPEND|O_CREAT);
-					argv[i]="";
 				}
-
 			}
 			else
 			{
-				//>input
 				 char* token = strtok(argv[i], ">");
-				 close(1);
 				 creat(token, 0666);
-				 argv[i]="";
 			}
+			argv[i] = "";
 		}
-		else if (n>=4 && strcmp(argv[i],"2>&1")==0)
+		else if (strcmp(argv[i],"2>&1") == 0 )
 		{
 			close(2);
 			dup(1);
-			argv[i]="";
+			argv[i] = "";
 		}
-		else if(n>=2 && argv[i][1]=='>')
+		else if (n >= 2 && argv[i][1] == '>' && (argv[i][0] == '1'  || argv[i][0] == '2' ) ) // 1> or 2>
 		{
-			if(argv[i][0]=='1')
-			{
-				if(n==2)
+				close(argv[i][0]-'0');
+				if( n == 2 )
 				{
-					close(1);
 					creat(argv[i+1], 0666);
-
-					argv[i]="";
-					i++;
-					argv[i]="";
+					argv[i++] = "";
 				}
 				else
 				{
 					 char* token = strtok(argv[i], ">");
 					 token = strtok(NULL, " ");
-					 close(1);
 					 creat(token, 0666);
-					 argv[i]="";
 				}
-
-			}
-			else if(argv[i][1]=='2')
-			{
-				if(n==2)
-				{
-					close(2);
-					creat(argv[i+1], 0666);
-
-					argv[i]="";
-					i++;
-					argv[i]="";	
-				}
-				else
-				{
-					char* token = strtok(argv[i], ">");
-					 token = strtok(NULL, " ");
-					 close(2);
-					 creat(token, 0666);
-					 argv[i]="";
-				}
-			}
+				argv[i] = "";
 		}
-		
-		else if(n>=1 && argv[i][0]=='<')
+		else if ( n >= 1 && argv[i][0] == '<') // <
 		{
+			close(0);
 			if(n==1)
 			{
-				close(0);
 				open(argv[i+1], O_RDONLY);
-
-				argv[i]="";
-				i++;
-				argv[i]="";
-
+				argv[i++]="";
 			}
 			else
 			{
 				char* token = strtok(argv[i], "<");
-				close(0);
 				open(token,O_RDONLY);
-				argv[i]="";
 			}
+			argv[i]="";
 		}
 	}
-	for(int i=0,j=0;i<=len;i++)
+
+	for (int i = 0,j = 0;i <= len ; i++ )
 	{
-		fin_argv[i]='\0';
-		if(strcmp(argv[i],"")!=0)
+		fin_argv[i] = '\0';
+		if ( strcmp(argv[i],"") != 0)
 		{
-			fin_argv[j]=argv[i];
+			fin_argv[j] = argv[i];
 			j++;
 		}
 	}
@@ -154,25 +107,26 @@ void execcmd(char exec_cmd[])
 {
 	char *argv[buf_size];
 	char* token = strtok(exec_cmd, " "); 
-	int idx=-1;
-	while(token != NULL)
+	int idx = -1;
+	while ( token != NULL )
 	{
 		idx++;
 		argv[idx] = token;
 		token = strtok(NULL, " ");
 	}
 	redirect(argv,idx);
+	if(argv[0] == NULL)
+		exit(0);
 	execvp(argv[0],argv);
 }
-void runcmd(char parsed_cmd[][buf_size],int idx)
+void runcmd(char parsed_cmd[][buf_size] , int idx)
 {
-	int r;
-	if(idx==-1)
+	if( idx == -1 )
 	{
 		exit(0);
 		return;
 	}
-	else if(idx==0)
+	else if (idx == 0 )
 	{
 		execcmd(parsed_cmd[idx]);
 	}
@@ -180,8 +134,8 @@ void runcmd(char parsed_cmd[][buf_size],int idx)
 	{
 	int fd[2];
 	pipe(fd);
-	int pid=fork();
-	if(pid==0)
+	int pid = fork();
+	if ( pid == 0 )
 	{
 
         close(fd[0]);
@@ -205,38 +159,35 @@ void parsecmd(char *cmd)
 {
 	char parsed_cmd[buf_size][buf_size];
 	char* token = strtok(cmd, "|"); 
-	int idx=-1;
-	while(token != NULL)
+	int idx = -1;
+	while ( token != NULL )
 	{
 		idx++;
 		strcpy(parsed_cmd[idx],token);
 		token = strtok(NULL, "|");
 	}
 	runcmd(parsed_cmd,idx);
-
-
-
 }
 int main()
 {
-	char cmd[buf_size],s[100];
-	while(getcmd(cmd)==1)
+	char cmd[buf_size];
+	while ( getcmd(cmd) == 1 )
 	{
 		int len = strlen(cmd);
-		if(cmd[len-1]==10)
-			cmd[len-1]='\0';
-		if(len>=4 && cmd[0]=='c' && cmd[1]=='d' && cmd[2] == ' ')
+		if ( cmd[len-1] == 10 )
+			cmd[len-1] = '\0';
+		if ( len >= 4 && cmd[0] == 'c' && cmd[1] == 'd' && cmd[2] == ' ' )
 		{
-			int x=chdir(cmd+3);
-			if(x<0)
+			int x = chdir(cmd+3);
+			if ( x < 0 )
 				printf("No such directory exist\n");
 		}
-		else if(len>=4 && cmd[0]=='e' && cmd[1]=='x' && cmd[2]=='i' && cmd[3]=='t')
+		else if ( len >= 4 && cmd[0] == 'e' && cmd[1] == 'x' && cmd[2] == 'i' && cmd[3] == 't' )
 			exit(0);
 		else
 		{
-			int pid=fork();
-			if(pid==0)
+			int pid = fork();
+			if( pid == 0 )
 			{
 				parsecmd(cmd);
 				printf("Invalid Command\n");
@@ -244,6 +195,5 @@ int main()
 			}
 			wait(NULL);
 		}
-
 	}
 }
