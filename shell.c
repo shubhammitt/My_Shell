dca00dba8e9389e79b1c6ecbcf80fca61845502b
Shubham Mittal <43808987+shubhammitt@users.noreply.github.com>
2020-03-04 15:24:34 +0530

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,1 +1,115 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <assert.h>
+#include <sys/stat.h>
+#define buf_size 256
+int  getcmd(char *cmd)
+{
+	fprintf(stdout, "\033[1;31mMy Shell   $ \033[0m");
+	memset(cmd,0,buf_size);
+	fgets(cmd,buf_size,stdin);
+	if(cmd[0]==0)
+		return 0;
+	return 1;
 
+}
+char* trim_front(char *s)
+{
+	while(strlen(s)>0 && s[0]==' ')
+	{
+		s++;
+	}
+	return s;
+}
+char** redirect(char **parsed_cmd,int idx)
+{
+	char argv[buf_size][buf_size];
+	trim_front(parsed_cmd[idx]);
+	return argv;
+}
+void execcmd(char exec_cmd[])
+{
+	char *argv[buf_size];
+	exec_cmd=trim_front(exec_cmd);
+	printf("%s\n",exec_cmd);
+	argv[0]=exec_cmd;
+	execvp(argv[0],argv);
+}
+void runcmd(char parsed_cmd[][buf_size],int idx)
+{
+
+	if(idx==-1)
+	{
+		return;
+	}
+	else if(idx==0)
+	{
+		execcmd(parsed_cmd[idx]);
+	}
+	else
+	{
+	int fd[2];
+	pipe(fd);
+	int pid=fork();
+	if(pid==0)
+	{
+        close(fd[0]);
+        close(1);
+        dup(fd[1]);
+        close(fd[1]);
+        runcmd(parsed_cmd,idx-1);
+	}
+	else
+	{
+		close(fd[1]);
+        close(0);
+        dup(fd[0]);
+        close(fd[0]);
+        execcmd(parsed_cmd[idx]);
+	}
+}
+}
+void parsecmd(char *cmd)
+{
+	char parsed_cmd[buf_size][buf_size];
+	char* token = strtok(cmd, "|"); 
+	int idx=-1;
+	while(token != NULL)
+	{
+		idx++;
+		strcpy(parsed_cmd[idx],token);
+		token = strtok(NULL, "|");
+	}
+	runcmd(parsed_cmd,idx);
+
+
+
+}
+int main()
+{
+	char cmd[buf_size],s[100];
+	while(getcmd(cmd)==1)
+	{
+		int len = strlen(cmd);
+		if(cmd[len-1]==10)
+			cmd[len-1]='\0';
+		if(len>=4 && cmd[0]=='c' && cmd[1]=='d' && cmd[2] == ' ')
+		{
+			chdir(cmd+3);
+		}
+		else
+		{
+			if(fork()==0)
+			{
+				parsecmd(cmd);
+			}
+			wait(NULL);
+		}
+
+	}
+}
